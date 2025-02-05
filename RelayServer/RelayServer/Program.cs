ef97fc45d83c320ce14b25718a7baf335f1b63f8
Fiidel <ada.helesic@gmail.com>
2025-02-05 20:31:30 +0100

    feat: CREATE and JOIN messages, room code generation

--- RelayServer/RelayServer/Program.cs
+++ RelayServer/RelayServer/Program.cs
@@ -13,19 +13,32 @@ app.Map("/connect", async (HttpContext context) =>
     if (context.WebSockets.IsWebSocketRequest)
     {
         using var webSocket = await context.WebSockets.AcceptWebSocketAsync();
-        while (true)
+        
+        bool joinedRoom = false;
+        while (!joinedRoom)
         {
-            var message = $"Datetime: {DateTime.Now}";
-            var bytes = Encoding.UTF8.GetBytes(message);
-            if (webSocket.State == WebSocketState.Open)
+            var buffer = new byte[1024];
+            var receiveResult = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            if (receiveResult.MessageType == WebSocketMessageType.Text)
             {
-                await webSocket.SendAsync(new ArraySegment<byte>(bytes), WebSocketMessageType.Text, true, CancellationToken.None);
+                var message = Encoding.UTF8.GetString(buffer, 0, receiveResult.Count);
+                var msgType = message.Split(':')[0];
+                var msgContent = message.Split(':')[1];
+
+                if (msgType == "CREATE")
+                {
+                    var roomCode = GenerateRoomCode();
+                    joinedRoom = true;
+                    await webSocket.SendAsync(new ArraySegment<byte>(Encoding.UTF8.GetBytes($"Room code: {roomCode}")), WebSocketMessageType.Text, true, CancellationToken.None);
+                }
+                else if (msgType == "JOIN")
+                {
+                    var roomCode = msgContent;
+                    Console.WriteLine($"Attempting to join room {roomCode}.");
+                    joinedRoom = true;
+                    await webSocket.SendAsync(new ArraySegment<byte>(Encoding.UTF8.GetBytes("Joined room.")), WebSocketMessageType.Text, true, CancellationToken.None);
+                }
             }
-            else if (webSocket.State == WebSocketState.Closed || webSocket.State == WebSocketState.Aborted)
-            {
-                break;
-            }
-            Thread.Sleep(1000);
         }
     }
     else
@@ -33,5 +46,18 @@ app.Map("/connect", async (HttpContext context) =>
         context.Response.StatusCode = StatusCodes.Status400BadRequest;
     }
 });
+
+string GenerateRoomCode()
+{
+    var symbolArray = "ABCDEFGHIJKLMNOPQRSTUVXYZ0123456789";
+    int codeLength = 8;
+    char[] roomCode = new char[codeLength];
+    Random random = new Random();
+    for (int i = 0; i < codeLength; i++)
+    {
+        roomCode[i] = symbolArray[random.Next(0, symbolArray.Length)];
+    }
+    return new string(roomCode);
+}
 
 app.Run();
