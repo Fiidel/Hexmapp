b596459212f6d7dbdafd19786ba072ee689d965b
Fiidel <ada.helesic@gmail.com>
2025-02-21 13:39:15 +0100

    feat(player map): UI tile selection

---
+++ hexmapp/PlayerMap/Scripts/PlayerMap.cs
@@ -0,0 +1,64 @@
+using Godot;
+using Godot.Collections;
+
+public partial class PlayerMap : Node2D
+{
+	[Export]
+	private ButtonGroup buttonGroup;
+	private Array<BaseButton> baseButtons;
+	private BaseButton selectedButton;
+	private TextureRect outline;
+
+	public override void _Ready()
+	{
+		outline = GetNode<TextureRect>("TerrainToolsUI/SelectionOutline");
+		if (outline == null)
+		{
+			GD.Print("Warning: Selection outline not found.");
+			return;
+		}
+
+		if (buttonGroup == null)
+		{
+			GD.Print("No terrain tool button group assigned!");
+			return;
+		}
+
+		baseButtons = buttonGroup.GetButtons();
+		if (baseButtons.Count == 0)
+		{
+			GD.Print("No terrain tool buttons loaded.");
+			return;
+		}
+		buttonGroup.Pressed += OnTerrainButtonPressed;
+
+		// select the first button
+		selectedButton = baseButtons[0];
+		// toggle
+		selectedButton.ButtonPressed = true;
+		// outline
+		outline.Visible = true;
+		MoveOutlineToButton(selectedButton);
+
+		GD.Print($"Selected tile {selectedButton.Name}");
+	}
+
+    private void OnTerrainButtonPressed(BaseButton button)
+    {
+        selectedButton = buttonGroup.GetPressedButton();
+		MoveOutlineToButton(selectedButton);
+		GD.Print($"Selected tile {selectedButton.Name}");
+    }
+
+	private void MoveOutlineToButton(BaseButton button)
+	{
+		outline.Reparent(button);
+		outline.Position = (button.Size - outline.Size) / 2;
+		// outline.Position = Vector2.Zero; // (0,0) local to the button == outline renders where the button renders
+	}
+
+
+	// Get debug print: "Selected tile {name of node}, of type {terrain tool type}"
+	// 1. get the type of the button from metadata (should I create a new class - type enum + path to texture?)
+	// 2. print the debug message
+}
